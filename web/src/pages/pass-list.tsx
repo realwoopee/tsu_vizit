bd28e10083863f16d7cb6903a14fa0abcb00ed21
Anisimov Aleksey <fallengalaxy5000@gmail.com>
2025-03-04 05:40:53 +0700

    Навигационная панель
    Каркас для навигационной панели. Разные кнопки для разных ролей.

--- web/src/pages/pass-list.tsx
+++ web/src/pages/pass-list.tsx
@@ -1,32 +1,35 @@
 import { PassListItem } from "../components/pass-list-item"
 import "../styles/pass-list.css"
+import { NavBar } from "../components/navigation/nav-bar"
 
 export const TestPage = () => {
     return (
-      <div className="test-container">
-        <PassListItem
-          status="На проверке"
-          fullName="Иванов Иван Иванович"
-          reason="Болезнь"
-          startDate="01.01.2024"
-          endDate="05.01.2024"
-        />
-        <PassListItem
-          status="Отклонено"
-          fullName="Иванов Иван Иванович"
-          reason="Болезнь"
-          startDate="01.01.2024"
-          endDate="05.01.2024"
-        />
-        <PassListItem
-          status="Принято"
-          fullName="Иванов Иван Ивановичччччччччччччччччччччччччччччччччччччччччччч"
-          reason="Болезнь"
-          startDate="01.01.2024"
-          endDate="05.01.2024"
-        />
-      </div>
-      
+      <>
+        <NavBar userRole="admin" />
+        <div className="test-container">
+          <PassListItem
+            status="На проверке"
+            fullName="Иванов Иван Иванович"
+            reason="Болезнь"
+            startDate="01.01.2024"
+            endDate="05.01.2024"
+          />
+          <PassListItem
+            status="Отклонено"
+            fullName="Иванов Иван Иванович"
+            reason="Болезнь"
+            startDate="01.01.2024"
+            endDate="05.01.2024"
+          />
+          <PassListItem
+            status="Принято"
+            fullName="Иванов Иван Ивановичччччччччччччччччччччччччччччччччччччччччччч"
+            reason="Болезнь"
+            startDate="01.01.2024"
+            endDate="05.01.2024"
+          />
+        </div>
+      </>
     )
   }
 

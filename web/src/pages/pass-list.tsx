391899b9d2b5865327401cd58de5e9815d951f22
Falexo1235 <165546517+Falexo1235@users.noreply.github.com>
2025-03-20 03:53:39 +0700

    Revert "Nav bar"

--- web/src/pages/pass-list.tsx
+++ web/src/pages/pass-list.tsx
@@ -1,35 +1,37 @@
 import { PassListItem } from "../components/pass-list-item"
 import "../styles/pass-list.css"
-import { NavBar } from "../components/navigation/nav-bar"
 
 export const TestPage = () => {
     return (
-      <>
-        <NavBar userRole="admin" />
-        <div className="test-container">
-          <PassListItem
-            status="На проверке"
-            fullName="Иванов Иван Иванович"
-            reason="Болезнь"
-            startDate="01.01.2024"
-            endDate="05.01.2024"
-          />
-          <PassListItem
-            status="Отклонено"
-            fullName="Иванов Иван Иванович"
-            reason="Болезнь"
-            startDate="01.01.2024"
-            endDate="05.01.2024"
-          />
-          <PassListItem
-            status="Принято"
-            fullName="Иванов Иван Ивановичччччччччччччччччччччччччччччччччччччччччччч"
-            reason="Болезнь"
-            startDate="01.01.2024"
-            endDate="05.01.2024"
-          />
-        </div>
-      </>
+      <div className="test-container">
+        <PassListItem
+          status="На проверке"
+          fullName="Николаев Николай Николаевич"
+          reason="Болезнь"
+          startDate="01.01.2024"
+          endDate="05.01.2024"
+          isEditable={false}
+        />
+        <PassListItem
+          status="Отклонено"
+          fullName="Иванов Иван Иванович"
+          reason="Семейные обстоятельства"
+          startDate="01.01.2024"
+          endDate="05.01.2024"
+          isEditable={true}
+          userRole="teacher"
+        />
+        <PassListItem
+          status="Принято"
+          fullName="Иванов Иван Ивановичччччччччччччччччччччччччччччччччччччччччччч"
+          reason="Учебная деятельность"
+          startDate="01.01.2024"
+          endDate="05.01.2024"
+          isEditable={true}
+          userRole="admin"
+        />
+      </div>
+      
     )
   }
 
